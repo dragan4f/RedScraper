45514abda4b2b31486928be27f06770f6597dbc9
dragan4f <draganaf.dev@outlook.com>
2025-01-11 13:17:45 +0100

    added cli

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ from pprint import pprint
 import json
 from bs4 import BeautifulSoup
 
+class Comment:
+    def __init__(self, subreddit, link, timestamp, content):
+        self.subreddit = subreddit
+        self.link = link
+        self.timestamp = timestamp
+        self.content = content
+
+    def __repr__(self):
+        return f"Subreddit: {self.subreddit}\nLink: {self.link}\nTimestamp: {self.timestamp}\nContent: {self.content}\n"
+
 async def scroll_to_bottom(page):
     cur_height = await page.evaluate("document.body.scrollHeight")
     new_height = 0
@@ -22,15 +32,13 @@ async def get_html_content(username):
     await page.wait_for("shreddit-profile-comment")
     await page.wait(0.1)
 
-    await scroll_to_bottom(page)
+    # await scroll_to_bottom(page)
 
     html_content = await page.get_content()
 
     return html_content
 
-def main():
-    username = "Venoosian"
-
+def get_user_comments(username):
     html_content = uc.loop().run_until_complete(
         get_html_content(username)
     )
@@ -47,15 +55,110 @@ def main():
         content = comment.find("div", class_="md").text.strip()
         datetime = comment.find("time").attrs['datetime']
 
-        processed_comments.append({
-            "subreddit": subreddit,
-            "link": full_link,
-            "timestamp": datetime,
-            "content": content
-        })
+        processed_comments.append(
+            Comment(subreddit, full_link, datetime, content)
+        )
 
-    # save to json
-    with open("comments.json", "w") as f:
-        json.dump(processed_comments, f, indent=4)
+    return processed_comments
 
-main()
+def filter_comments(comments, keyword):
+    filtered_comments = []
+
+    for comment in comments:
+        if keyword.lower() in comment.content.lower():
+            filtered_comments.append(comment)
+
+    return filtered_comments
+
+def filter_subreddits(comments, subreddits):
+    filtered_comments = []
+
+    for comment in comments:
+        if comment.subreddit in subreddits:
+            filtered_comments.append(comment)
+
+    return filtered_comments
+
+def filter_date_range(comments, start_date, end_date):
+    filtered_comments = []
+
+    for comment in comments:
+        date = comment.timestamp.split("T")[0]
+        if start_date <= date <= end_date:
+            print(date, "match")
+            filtered_comments.append(comment)
+
+    return filtered_comments
+
+def cli():
+    help_message = """
+    Enter 'exit' to exit the program.
+    Enter 'help' to view this menu.
+    Enter 'get {username}' to get comments of a user.
+    Enter 'show' to display all comments.
+    Enter 'save' to save comments to a file.
+    Enter 'filter key {keyword}' to filter comments by keyword.
+    Enter 'filter sub {subreddit}' to filter comments by subreddit.
+    Enter 'filter date {start_date} {end_date}' to filter comments by date range.
+    """
+    
+    print("Welcome to Reddit Comment Scraper!")
+    print(help_message)
+
+    user_comments = []
+
+    while True:
+        command = input("Enter command: ")
+
+        if command == "exit":
+            exit()
+
+        elif command == "help":
+            print(help_message)
+
+        elif command.startswith("get"):
+            username = command.split(" ")[1]
+            user_comments = get_user_comments(username)
+
+        elif command == "show":
+            pprint(user_comments)
+
+        elif command == "save":
+            with open("comments.json", "w") as f:
+                json.dump(user_comments, f, indent=4)
+            print("Comments saved to comments.json.")
+
+        elif command.startswith("filter"):
+            if len(user_comments) == 0:
+                print("No comments to filter. Enter 'get {username}' to get comments of a user.")
+                continue
+
+            filter_type = command.split(" ")[1]
+
+            if filter_type == "key":
+                keyword = command.split(" ")[2]
+                print(keyword)
+                pprint(
+                    filter_comments(user_comments, keyword)
+                )
+
+            elif filter_type == "sub":
+                subreddit = command.split(" ")[2]
+                pprint(
+                    filter_subreddits(user_comments, subreddit)
+                )
+
+            elif filter_type == "date":
+                start_date = command.split(" ")[2]
+                end_date = command.split(" ")[3]
+                pprint(
+                    filter_date_range(user_comments, start_date, end_date)
+                )
+
+            else:
+                print("Invalid filter type. Enter 'help' to view the menu.")
+
+        else:
+            print("Invalid command. Enter 'help' to view the menu.")
+
+cli()
